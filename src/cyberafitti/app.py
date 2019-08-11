f0f614882035ac890d24daa9f11feb0ab9a7eb44
wodmsqkr <jw_1995@naver.com>
2019-08-11 18:11:25 +0900

    준비중입니다 적용

--- src/cyberafitti/app.py
+++ src/cyberafitti/app.py
@@ -3,29 +3,39 @@ from run_model import run_model
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/youtube', methods=['GET', 'POST'])
 def youtube():
-    return render_template('youtube.html')
+    # return render_template('youtube.html')
+    return render_template('prepare.html')
+
 
 @app.route('/afreeca', methods=['GET', 'POST'])
 def afreeca():
-    return render_template('afreeca.html')
+    # return render_template('afreeca.html')
+    return render_template('prepare.html')
+
 
 @app.route('/twitch', methods=['GET', 'POST'])
 def twitch():
-    return render_template('twitch.html')
+    # return render_template('twitch.html')
+    return render_template('prepare.html')
+
 
 @app.route('/model')
 def model():
     return render_template('model.html')
 
+
 @app.route('/download')
 def download():
     return render_template('download.html')
+
 
 @app.route('/demo', methods=['GET', 'POST'])
 def demo():
@@ -35,6 +45,7 @@ def demo():
         return result
     else:
         return render_template('demo.html')
+
 
 if __name__ == '__main__':
     app.run()
